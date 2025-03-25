653e016153ada018db56d9ae7a44d2c29d84ae8e
MaryelN <maryel.navarromtz@gmail.com>
2025-03-25 10:12:50 +0100

    Creation of Navbar

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,10 @@
+import Navbar from "../components/Navbar/Navbar";
 function PageNotFound() {
-  return <div>Page Not Found</div>;
+  return (
+    <div>
+      Page Not Found <Navbar />
+    </div>
+  );
 }
 
 export default PageNotFound;

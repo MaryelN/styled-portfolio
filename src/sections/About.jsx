aa0434d18b527071dd0f608d119c08d6df8869b2
Cédric Phung <cedric3011@outlook.fr>
2025-03-26 13:30:23 +0100

    add div

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,15 +1,12 @@
-import styled from "styled-components";
 import Techno from "../components/Techno";
 import TextAbout from "../components/TextAbout";
 
-const StyledAbout = styled.div``;
-
 function About() {
   return (
-    <StyledAbout>
+    <div>
       <TextAbout />
       <Techno />
-    </StyledAbout>
+    </div>
   );
 }
 

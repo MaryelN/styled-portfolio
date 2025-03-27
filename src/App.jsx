acf00bd81df17f57f8ba3327d9798ea1fd8d6d0b
MaryelN <maryel.navarromtz@gmail.com>
2025-03-28 00:24:17 +0100

    Resolve merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Hero from "./sections/Hero";
 import Formation from "./sections/Formation";
 import Navbar from "./components/Navbar/Navbar";
+import About from "./sections/About";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route target="/#Hero" element={<Hero />}></Route>
+        <Route target="/#About" element={<About />}></Route>
         <Route target="/#formation" element={<Formation />} />
       </Routes>
       <Navbar />
       <Hero />
+      <About />
       <Formation />
     </BrowserRouter>
   );
---
+++ src/sections/About.jsx
@@ -0,0 +1,13 @@
+import Techno from "../components/Techno";
+import TextAbout from "../components/TextAbout";
+
+function About() {
+  return (
+    <div id="about">
+      <TextAbout />
+      <Techno />
+    </div>
+  );
+}
+
+export default About;

479f18c7c7edb0be76dac462f827dbb1511147dd
Cédric Phung <cedric3011@outlook.fr>
2025-03-31 10:32:06 +0200

    add closing tag

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import About from "./sections/About";
 import Hero from "./sections/Hero";
 import Formation from "./sections/Formation";
 import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
       <Navbar />
       <Hero />
       <About />
-    </>
+      <Formation />
+    </BrowserRouter>
   );
 }
 
